d438c6d9cffc9fc1f44db68e75f410aea91754e2
Yuki Nagato <46703015+YukiNagatoVR@users.noreply.github.com>
2022-05-10 16:14:28 +1000

    Update AutoMatcher.cs

--- Assets/MaximalMatching/AutoMatcher.cs
+++ Assets/MaximalMatching/AutoMatcher.cs
@@ -286,7 +286,7 @@ public class AutoMatcher : UdonSharpBehaviour
                 
                 if (lightningEnabled = false)
                 {
-                    if (gameVariant = 1)
+                    if (gameVariant == 1)
                     {   
                     var nextVariant = 2;    
                     }
